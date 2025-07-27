fbabeeb064c8183dcaea72deb7301147a5862e13
Faheem Iqbal <zargarfaheem4229@gmail.com>
2025-07-27 15:57:53 +0530

    Q3

---
+++ Q3.cpp
@@ -0,0 +1,43 @@
+// The prime factors of 13195 are 5, 7, 13 and 29.
+// What is the largest prime factor of the number 600851475143
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+vector<long long> primeFactors(long long n) {
+    vector<long long> factors;
+
+    for (long long i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            factors.push_back(i);
+            while (n % i == 0) {
+                n /= i;
+            }
+        }
+    }
+
+    if (n > 1) {
+        factors.push_back(n);
+    }
+
+    return factors;
+}
+
+long long largestPrimeFactor(long long n) {
+    vector<long long> factors = primeFactors(n);
+    if (factors.empty()) {
+        return n; 
+    }
+    return *max_element(factors.begin(), factors.end());
+}
+
+int main() {
+    long long n = 600851475143;
+    cout << "Largest prime factor: " << largestPrimeFactor(n) << endl;
+    return 0;
+}
+
